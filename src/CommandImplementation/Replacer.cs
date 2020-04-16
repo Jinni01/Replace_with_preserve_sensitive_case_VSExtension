a97ec4c566a064511315695f3d372035d4927832
Kusinaga <dml21@naver.com>
2020-04-16 13:21:10 +0900

    initial commit

---
+++ src/CommandImplementation/Replacer.cs
@@ -0,0 +1,61 @@
+﻿/***************************************************************************
+
+Copyright (c) Microsoft Corporation. All rights reserved.
+THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+
+***************************************************************************/
+
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Operations;
+using System.Text.RegularExpressions;
+using PromptGenerator;
+
+namespace ReplaceCommandImplementation
+{
+    public static class Replacer
+    {
+        public static void ReplaceSelectedLines(ITextView textView, IEditorOperations editorOperations)
+        {
+            if (textView.Selection.IsEmpty)
+            {
+                return;
+            }
+
+            var selectedSpan = textView.Selection.SelectedSpans[0];
+
+            string origin, change;
+            string title = "Replace With Preserve Sensitive Case";
+
+            origin = Prompt.ShowDialog("Replace From : ", title);
+            if (origin.Length == 0) return;
+
+            change = Prompt.ShowDialog("Replace To : ", title);
+            if (origin.Length != change.Length) return;
+
+            string trans = Regex.Replace(selectedSpan.GetText(), origin,
+                new MatchEvaluator((Match match) =>
+                {
+                    string val = match.Value;
+                    char[] arrayVal = val.ToCharArray();
+
+                    int len = val.Length;
+                    for (int i = 0; i < len; ++i)
+                    {
+                        if (char.IsUpper(arrayVal[i])) arrayVal[i] = char.ToUpper(change[i]);
+                        else arrayVal[i] = char.ToLower(change[i]);
+                    }
+
+                    return new string(arrayVal);
+                })
+                , RegexOptions.IgnoreCase);
+
+            textView.TextBuffer.Replace(selectedSpan, trans);
+            //editorOperations.MoveToEndOfLine(extendSelection: false);
+            //editorOperations.Delete();
+            editorOperations.DeleteHorizontalWhiteSpace();
+        }
+    }
+}
---
+++ src/ModernCommandHandler/ReplaceCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿/***************************************************************************
+
+Copyright (c) Microsoft Corporation. All rights reserved.
+THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+
+***************************************************************************/
+
+using ReplaceCommandImplementation;
+using Microsoft.VisualStudio.Commanding;
+using Microsoft.VisualStudio.Text.Operations;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+
+namespace ModernCommandHandler
+{
+    [Export(typeof(ICommandHandler))]
+    [ContentType("text")]
+    [Name(nameof(ReplaceCommandHandler))]
+    public class ReplaceCommandHandler : ICommandHandler<ReplaceCommandArgs>
+    {
+        public string DisplayName => "Replace With Preserve Sensitive Case in Selected Range";
+
+        [Import]
+        private IEditorOperationsFactoryService EditorOperations = null;
+
+        public CommandState GetCommandState(ReplaceCommandArgs args)
+        {
+            return args.TextView.Selection.IsEmpty ? CommandState.Unavailable : CommandState.Available;
+        }
+
+        public bool ExecuteCommand(ReplaceCommandArgs args, CommandExecutionContext context)
+        {
+            using (context.OperationContext.AddScope(allowCancellation: false, description: "Replace With Preserve Sensitive Case in Selected Range"))
+            {
+                //args.TextView.TextBuffer.Insert(0, "// Invoked from modern command handler\r\n");
+                Replacer.ReplaceSelectedLines(args.TextView, EditorOperations.GetEditorOperations(args.TextView));
+            }
+
+            return true;
+        }
+    }
+}
